89a684de6e64b39f6f7db8f96006474929b6a7b1
Sebastian Olsson <sebelino7@gmail.com>
2017-03-23 19:18:05 +0100

    Added vector operator<< test.

--- simpletests/test.cpp
+++ simpletests/test.cpp
@@ -1,6 +1,14 @@
 #include <gtest/gtest.h>
 #include "../main.h"
 
+TEST(VectorTest, StringRepresentation) {
+    Vector v;
+    std::stringstream stream;
+    stream << v;
+    std::string repr = stream.str();
+    ASSERT_EQ(repr, "[]");
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
