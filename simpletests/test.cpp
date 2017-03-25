85fffc029e0e8a223d95f9455a626a3fc45b39c4
Sebastian Olsson <sebelino7@gmail.com>
2017-03-25 23:37:17 +0100

    Working on parametrized test.

--- simpletests/test.cpp
+++ simpletests/test.cpp
@@ -1,8 +1,21 @@
 #include <gtest/gtest.h>
 #include "../main.h"
 
-TEST(VectorTest, StringRepresentation) {
+TEST(VectorTest, StringRepresentationEmpty) {
     Vector v;
+    std::stringstream stream;
+    stream << v;
+    std::string repr = stream.str();
+    ASSERT_EQ(repr, "[]");
+}
+
+/*
+ * @param ({}, "[]")
+ * @param ({77}, "[77]")
+ * @param ({77, 3}, "[77, 3]")
+ */
+TEST(VectorTest, StringRepresentation) {
+    Vector v({});
     std::stringstream stream;
     stream << v;
     std::string repr = stream.str();
