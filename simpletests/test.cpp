b688afbf90cfd1de8fc70b08c4ce463cffa8aa68
Sebastian Olsson <sebelino7@gmail.com>
2017-03-28 23:45:38 +0200

    Parameterized test still not quite working...

--- simpletests/test.cpp
+++ simpletests/test.cpp
@@ -1,6 +1,25 @@
 #include <gtest/gtest.h>
 #include "../main.h"
 
+class TestFixture : public ::testing::TestWithParam<std::tuple<int, std::string>> {};
+
+/*
+* @param ({}, "[]")
+* @param ({77}, "[77]")
+* @param ({77, 3}, "[77, 3]")
+*/
+TEST_P(TestFixture, StringRepresentation) {
+    std::string expected = std::get<0>(GetParam());
+    std::string returned = std::get<1>(GetParam());
+    Vector v({}});
+    std::stringstream stream;
+    stream << v;
+    std::string repr = stream.str();
+    ASSERT_EQ(repr, "[]");
+}
+
+INSTANTIATE_TEST_CASE_P(VectorTest, TextFixture, ::testing::Values(std::make_tuple("meeny", "meeny"), std::make_tuple("miny", "miny")));
+
 TEST(VectorTest, StringRepresentationEmpty) {
     Vector v;
     std::stringstream stream;
@@ -9,20 +28,12 @@ TEST(VectorTest, StringRepresentationEmpty) {
     ASSERT_EQ(repr, "[]");
 }
 
-/*
- * @param ({}, "[]")
- * @param ({77}, "[77]")
- * @param ({77, 3}, "[77, 3]")
- */
-TEST(VectorTest, StringRepresentation) {
-    Vector v({});
-    std::stringstream stream;
-    stream << v;
-    std::string repr = stream.str();
-    ASSERT_EQ(repr, "[]");
-}
 
 int main(int argc, char **argv) {
+    for(int i = 0; i < 100; ++i) {
+        numbers.push_back(i);
+        strings.push_back("hoy");
+    }
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
